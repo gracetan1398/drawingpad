0b4f112731e097aa4a66ba1f0f2f3148e073dfe6
grace <gracetan1398@gmail.com>
2016-08-05 14:00:19 -0500

    Drawing Pad

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-var myRed = 10;
+var myRed = 255;
 var myGreen = 0;
-var myBlue = 225;
+var myBlue = 200;
 
 function setup() {
   createCanvas(640, 480);
-  background(255, 0, 102);
+  background(255, 255, 255);
 }
 
 function draw() {
@@ -13,16 +13,20 @@ function draw() {
   ellipse(mouseX, mouseY, 25, 25);
   myRed = myRed + 0.5;
   myBlue = myBlue - 1;
+  
 if (myRed >=255){
-  myRed = 0;
+  myRed = 100;
 }
 
 if (myBlue >=255){
-  myBlue = 255;
+  myBlue = 100;
+}
+
+if (mouseIsPressed == true) {
+  noStroke();
+  fill (255, 255, 255);
+  ellipse(mouseX, mouseY, 25, 25);
 }
 }
 
-function mousePressed(){
-background(255, 0, 102);
-}
 
